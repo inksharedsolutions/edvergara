69377f4fd63d0e5d3ee5cc05f81d2c906f9adb32
inksharedsolutions <webdev.inkshared@gmail.com>
2020-08-12 03:00:11 +0800

    changes-8-12-2020

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import * as THREE from 'three'
 import {Link} from 'gatsby'
+import NET from 'vanta/dist/vanta.net.min'
+
 
 
 const Banner = ()=>{
+
+  const [vantaEffect, setVantaEffect] = useState(0)
+  const myRef = useRef(null)
+
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(NET({
+          el: myRef.current,
+          THREE: THREE, 
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          color: 0x4d4d4d,
+          backgroundColor: 0x222222,
+          points: 11.00,
+          maxDistance: 25.00,
+          spacing: 16.00
+      }))
+    }
+    return () => {
+      //Destroy Effects..
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
+
+
     return (
         <>
-            <section className="banner-section">
+            <section className="banner-section" ref={myRef}>
                 <div className="container-gt">
                     <div className="two-grid-columns">
                     
